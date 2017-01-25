29eefdbc2b45c3daa6e9d84ed24ba036b96d47d2
Luke Weber <lukedottec@gmail.com>
2017-01-25 07:30:36 -0800

    Added heapsort; now, gotta test

--- wordFreqCounter.js
+++ wordFreqCounter.js
@@ -2,51 +2,108 @@
 
 // Luke Weber
 // 01/24/2017
-// Pramp Interview
+// Improved from scan_1 to scan_2
+
+
+/* scan_2 */
 
 /**
  *  Return unique words and the number of their occurences
- *  @param {string} doc - Linear combination of space-seperated words
- *  @returns {Array} Array of unique words paired with their frequency
+ *  @param {String} doc - Linear combination of space-seperated words
+ *  @returns {Array} Unique words paired with their frequency
  */ 
-function scan(doc) {
+function scan_2(doc) {
 
-  var numWords = 0;
+  // Capture word frequencies
+  let dict = {};
+  doc.split(" ").forEach(function(word) {
+    dict[word] = (dict[word] || 0) + 1; 
+  });
 
-  // Loop doc char-by-char
-  var i = str.length;
-  var word;
-  while (i--) {
-    var val = str[i];
+  // Dictionary to array; used for sorting!
+  let dictArr = Object.keys(dict).map(k => [k, dict[k]);
 
-    if (val == " ") {
-      // TODO
+  // Heap sort
+  let heap = new MaxHeap();
+  heap.sort(dictArr);
 
-      word = "";
-      numWords++;
-    } else {
-      word += val;
+  return dictArr;
+}
+
+class MaxHeap {
+  constructor() { 
+    this.d = 2;
+  }
+
+  iParent(i) { return Math.floor((i - 1) / this.d); }
+  /* @param {Number} c - Relative child index, i.e. 0 <= c < d */
+  iChild(i, c) { return (this.d * i) + (c + 1); }
+
+  // Heapify given array
+  ify(arr, count) {
+    // Parent of last element
+    let start = this.iParent(count - 1); 
+
+    // Right-to-left sifting down
+    while (start >= 0) {
+      this.siftDown(arr, start, count - 1); 
+      start--;
     }
   }
   
-  numWords++;
-  // TODO
+  siftDown(arr, start, end) {
+    let root = start;
+
+    while (this.iChild(root, 0) <= end) {
+      let child = this.iChild(root, 0);
+      let swap = root;
+
+      if (arr[swap] < arr[child]) {
+        swap = child;
+      } 
+      if (((child + 1) <= end) && (arr[swap] < arr[child + 1])) {
+        swap = child + 1;
+      }
+      if (swap = root) {
+        return;
+      } else {
+        swap(root, swap);
+        root = swap;
+      }
+    }
+  }
+
+  // Heapsort on given array
+  sort(arr, count) {
+    // Initial call from outside
+    if (arguments.length === 1) { count = arr.length; } 
+
+    let end = count - 1;
+    while (end > 0) {
+      // Move max to front of sorted elements
+      swap(end, 0);
+      // Heap size reduced
+      end--;
+      // Restore heap
+      siftDown(arr, 0, end);
+    }
+  }
+
+  swap(arr, a, b) {
+    let c = arr[a];
+    arr[a] = arr[b];
+    arr[b] = c;
+  } 
 }
 
-/**
- *  Return unique words and the number of their occurences
- *  @param {string} doc - Linear combination of space-seperated words
- *  @returns {Array} Array of unique words paired with their frequency
- */ 
-function wordHash(word) {
 
-}
+/* scan_1 */
 
 // Input: string doc, containing space-seperated words
 // Output: dictionary containing unique words as keys and number of occurances as values
 // Time: O(n + n + n^2) = O(n^2) ... pretty bad
 // Space: O(n + n + n) = O(n) ... alright
-function scan(doc) {
+function scan_1(doc) {
    // Get words in array
    var words = doc.split(' ');
    var dict = {};
@@ -58,7 +115,6 @@ function scan(doc) {
       if (dict[word] == undefined) { 
          dict[word] = 0;
       }
-      
       dict[word] += 1;
    });
    
